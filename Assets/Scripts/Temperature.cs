3324b655c2663d467f2b2185fa13f8f46168f95c
Angela Dai <zhjh.angela@gmail.com>
2022-11-27 20:03:26 -0500

    add ar camera

---
+++ Assets/Scripts/Temperature.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temperature : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float areaAbs;
+    float areaRads;
+    float planetAlbedo;
+    float emissivity;
+    float luminosity;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float EffectiveTemp()
+    {
+        return 0;
+    }
+}
